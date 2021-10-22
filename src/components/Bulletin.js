a136e0e2ea5d9745f0911c05e7ce736775bbeded
Jasperora <robert852654@gmail.com>
2021-10-22 20:58:58 +0800

    add shell

--- src/components/Bulletin.js
+++ src/components/Bulletin.js
@@ -1,26 +1,41 @@
 import React from 'react'
-import { Table, Tag, TagGroup, Button, Panel, Drawer } from 'rsuite';
-import { isMobile } from "react-device-detect";
+import { Table, Tag, TagGroup, Button, Panel, Drawer } from 'rsuite'
+import { isMobile } from 'react-device-detect'
+import { pageQuery } from '../pages/index.js'
 
-const { Column, HeaderCell, Cell, Pagination } = Table;
+const { Column, HeaderCell, Cell, Pagination } = Table
 
-export default () => (
-  <div>
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>departmentTag</th>
-            <th>announcement type tag</th>
-            <th>create time</th>
-            <th>post</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-        </tbody>
-      </table>
-    </div>
-  </div>
-)
+export default class Bulletin extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <h2>消息公告</h2>
+          </div>
+          <Panel bordered>
+            <Table width={isMobile ? 320 : undefined}>
+              <Column width={120}>
+                <HeaderCell>Tags</HeaderCell>
+                <Cell dataKey="tags"></Cell>
+              </Column>
+              {!isMobile ? (
+                <Column width={150}>
+                  <HeaderCell>Date</HeaderCell>
+                  <Cell dataKey="updatedAt" />
+                </Column>
+              ) : (
+                ''
+              )}
+
+              <Column width={!isMobile ? 400 : 180}>
+                <HeaderCell>Title</HeaderCell>
+                <Cell dataKey="title" />
+              </Column>
+            </Table>
+          </Panel>
+        </div>
+      </div>
+    )
+  }
+}
