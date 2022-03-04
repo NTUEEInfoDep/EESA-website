987bde26fc3f76bcb79a7f653aae1b722639e034
connor226 <connor.taipei@gmail.com>
2022-03-04 20:43:55 +0800

    department page combine leadercard

--- src/pages/department.js
+++ src/pages/department.js
@@ -3,22 +3,11 @@ import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Typography from '@mui/material/Typography'
 import CardMedia from '@mui/material/CardMedia'
-import CardContent from '@mui/material/CardContent'
-import CardHeader from '@mui/material/CardHeader'
 import Card from '@mui/material/Card'
 import Container from '@mui/material/Container'
-import Chip from '@mui/material/Chip'
 import Grid from '@mui/material/Grid'
-import Stack from '@mui/material/Stack'
-import Layout from '../layouts/department'
 import LeaderCards from '../components/leaderCard'
 import Box from '@mui/material/Box'
-import Avatar from '@mui/material/Avatar'
-//
-import SpeedDial from '@mui/material/SpeedDial'
-import SpeedDialAction from '@mui/material/SpeedDialAction'
-//  import Layout from '../layouts/department'
-//  import LeaderCard from '../components/leadercard';
 
 const actions = ['資訊部', '學術部', '衛生部', '行銷部', 'EE+']
 
@@ -34,107 +23,27 @@ class department extends Component {
           image={depinfo.backgroundImage.file.url}
           alt="dep-image"
         />
-        <CardContent>
-          <div>
-            <SpeedDial
-              ariaLabel="SpeedDial basic trial"
-              direction="right"
-              FabProps={{
-                variant: 'extended',
-                size: 'large',
-              }}
-              icon={
-                <Typography
-                  variant="h5"
-                  className="section-headline"
-                  sx={{ padding: '0' }}
-                >
-                  {depinfo.name}
+        <Typography variant="h2" className="section-headline">
+          {depinfo.name}
+        </Typography>
+        <Box style={{ backgroundColor: 'dimgrey' }}>
+          <Grid container>
+            <Grid item xs={12}>
+              <LeaderCards depinfo={depinfo} />
+            </Grid>
+            <Grid item xs={12}>
+              <Card sx={{ margin: '5px', padding: '15px' }}>
+                <Typography variant="body2">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: depinfo.intro.childMarkdownRemark.html,
+                    }}
+                  />
                 </Typography>
-              }
-            >
-              {actions
-                .filter((action) => {
-                  return action !== depinfo.name
-                })
-                .map((action) => {
-                  return (
-                    <SpeedDialAction
-                      key={action}
-                      FabProps={{
-                        variant: 'extended',
-                        size: 'large',
-                        sx: {
-                          bgcolor: '#34aaff',
-                          '&:hover': {
-                            bgcolor: 'primary.main',
-                            color: 'white',
-                          },
-                        },
-                      }}
-                      tooltipPlacement="bottom"
-                      icon={
-                        <Typography
-                          variant="h5"
-                          className="section-headline"
-                          sx={{ padding: '0' }}
-                        >
-                          {action}
-                        </Typography>
-                      }
-                    />
-                  )
-                })}
-            </SpeedDial>
-            <Typography variant="h2" className="section-headline">
-              {depinfo.name}
-            </Typography>
-            <Box style={{ backgroundColor: 'dimgrey' }}>
-              <Grid container>
-                <Grid xs={9}>
-                  <Card sx={{ margin: '5px', padding: '15px' }}>
-                    <Typography variant="body2">
-                      <div
-                        dangerouslySetInnerHTML={{
-                          __html: depinfo.intro.childMarkdownRemark.html,
-                        }}
-                      />
-                    </Typography>
-                  </Card>
-                </Grid>
-                <Grid xs={3}>
-                  {depinfo.leaders.map((leader) => {
-                    return (
-                      <Card sx={{ margin: '5px' }}>
-                        <CardHeader
-                          avatar={
-                            <Avatar
-                              src={leader.leaderSelfie.file.url}
-                              alt={leader.leaderName}
-                              sx={{ width: 80, height: 80 }}
-                            />
-                          }
-                          title={
-                            <CardContent>
-                              <Chip
-                                color="primary"
-                                label={leader.title}
-                                variant="subtitle1"
-                              />
-                              <Typography variant="h6" color={'#34aaff'}>
-                                {leader.leaderName}
-                              </Typography>
-                            </CardContent>
-                          }
-                        />
-                      </Card>
-                    )
-                  })}
-                </Grid>
-              </Grid>
-            </Box>
-          </div>
-        </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
       </Container>
       //  </Layout>
     )
