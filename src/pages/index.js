36ba660e122d5d2afeca80e7e782574ed42717c1
Hong-En Chen <hong-enchen@Anonymous-3.local>
2022-04-22 19:36:57 +0800

    fix blog post issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import get from 'lodash/get'
 import { Helmet } from 'react-helmet'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
 import TimeLine from '../components/timeline'
 import TimeLine3 from '../components/timeline3'
 import EventLine from '../components/EventLine/EventLine'
@@ -13,28 +12,18 @@ import Bulletin from '../components/Bulletin'
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
     const [author] = get(this, 'props.data.allContentfulPerson.edges')
     const activities = get(this, 'props.data.allContentfulActivity.edges')
-    const bulletin = get(this, 'props.data.allContentfulBulletinBoard.edges[0]')
+    const bulletin = get(this, 'props.data.allContentfulBlogPosts.edges')
 
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
-          <Bulletin data={bulletin.node} />
+          <Bulletin data={bulletin} />
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
           </div>
           <TimeLine data={activities} />
           <TimeLine3 data={activities} />
@@ -63,23 +52,26 @@ export const pageQuery = graphql`
         }
       }
     }
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+    allContentfulBlogPosts(sort: { fields: [publishDate], order: DESC }) {
       edges {
         node {
           title
           slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+          publishDate(formatString: "MMM Do, YYYY")
+          tag
+          titleImage {
+            fluid(maxWidth: 100, maxHeight: 196, resizingBehavior: SCALE) {
               ...GatsbyContentfulFluid_tracedSVG
             }
           }
           description {
-            childMarkdownRemark {
-              html
+            content {
+              content {
+                value
+              }
             }
           }
+          departments
         }
       }
     }
