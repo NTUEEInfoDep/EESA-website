96ba7d93ea3e902c5a6d8d06a3360b7d026638bb
Mecoli1219 <michaellai901026@gmail.com>
2022-04-29 19:19:03 +0800

    modify notation

--- src/static/constant.js
+++ src/static/constant.js
@@ -2,7 +2,7 @@ import TimeLine3 from '../components/timeline3'
 import Bulletin from '../components/Bulletin'
 
 // A is the value of the Body of the Department Main Page on contentful
-// A: [B, C]    =>  <A data={get(this, `props.data.allContentful${B}.edges`)}
+// A: [B, C]    =>  <C data={get(this, `props.data.allContentful${B}.edges`)} />
 
 const FUNC_CONST = {
   Activity: ['Activity', TimeLine3], // <TimeLine data={get(this, 'props.data.allContentfulActivity.edges')} />
