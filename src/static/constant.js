2d8a46466db52b1f325b9adbaafe1339a66803a1
Mecoli1219 <michaellai901026@gmail.com>
2022-05-06 20:56:40 +0800

    EESA-#3. add some comment

--- src/static/constant.js
+++ src/static/constant.js
@@ -2,14 +2,15 @@ import TimeLine3 from '../components/timeline3'
 import Bulletin from '../components/Bulletin'
 
 // A is the value of the Body of the Department Main Page on contentful
-// A: [B, C]    =>  <C data={get(this, `props.data.allContentful${B}.edges`)} />
+// A: [B, C, D]    =>  <C data={get(this, `props.data.allContentful${B}.edges`)} />
+// D is the title of the component. If not want to use it, keep it an empty string('')
 
 const FUNC_CONST = {
-  Activity: ['Activity', TimeLine3, 'Activity'], // <TimeLine data={get(this, 'props.data.allContentfulActivity.edges')} />
+  Activity: ['Activity', TimeLine3, ''], // <TimeLine data={get(this, 'props.data.allContentfulActivity.edges')} />
 }
 
 const CLASS_CONST = {
-  Bulletin: ['BlogPosts', Bulletin, 'Bullitin'], // <Bulletin data={get(this, 'props.data.allContentfulBlogPosts.edges')} />
+  Bulletin: ['BlogPosts', Bulletin, 'Bulletin'], // <Bulletin data={get(this, 'props.data.allContentfulBlogPosts.edges')} />
 }
 
 export { FUNC_CONST, CLASS_CONST }
