0a6c7481c34cb40fd8eef8db694e730251fff305
caffrey928 <caffreyacnologialawace@gmail.com>
2022-05-06 18:08:43 +0800

    judge blog post tag

--- src/components/timeline3.js
+++ src/components/timeline3.js
@@ -18,8 +18,8 @@ const TimeLineItem = ({ date, title, content, short, image }) => {
         borderTop: '3px solid rgb(33, 150, 243)',
         background: 'rgb(250, 250, 250)',
         color: 'gray',
-        display: 'grid', 
-        gridTemplateColumns: "75% 25%",
+        display: 'grid',
+        gridTemplateColumns: '75% 25%',
       }}
       contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
       date={date}
@@ -32,7 +32,7 @@ const TimeLineItem = ({ date, title, content, short, image }) => {
         <h4 className="vertical-timeline-element-subtitle">{short}</h4>
         <p>{content}</p>
       </div>
-    
+
       <div>
         <Img fluid={image}></Img>
       </div>
@@ -43,16 +43,20 @@ const TimeLineItem = ({ date, title, content, short, image }) => {
 export default function TimeLine3({ data }) {
   return (
     <VerticalTimeline lineColor={'grey'} layout="1-column-left">
-      {data.map((element) => (
-        <TimeLineItem
-          date={element.node.publishDate}
-          title={element.node.title}
-          content={element.node.description.content[0].content[0].value}
-          short={element.node.slug}
-          image={element.node.titleImage.fluid}
-          key={element.node.title}
-        />
-      ))}
+      {data.map((element) => {
+        const match = (element.node.tag.indexOf('重要') > -1)
+        if(!match) return
+        return (
+          <TimeLineItem
+            date={element.node.publishDate}
+            title={element.node.title}
+            content={element.node.description.content[0].content[0].value}
+            short={element.node.slug}
+            image={element.node.titleImage.fluid}
+            key={element.node.title}
+          />
+        )
+      })}
       <VerticalTimelineElement
         iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
         icon={<StarIcon />}
